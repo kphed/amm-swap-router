6aa16dcea0753d4324140eb69ed8e51838ddc3bb
kphed <kpcrypto69@gmail.com>
2023-08-17 09:28:56 -0400

    Add initial PoolRegsitry with pool setter

--- src/PoolRegistry.sol
+++ src/PoolRegistry.sol
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-contract PoolRegistry {}
+import {EnumerableMap} from "openzeppelin/utils/structs/EnumerableMap.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
+
+contract PoolRegistry is Ownable {
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
+
+    EnumerableMap.AddressToUintMap private _pools;
+
+    event SetPool(
+        address indexed pool,
+        address indexed poolInterface,
+        bool indexed isNew
+    );
+
+    constructor(address initialOwner) {
+        _initializeOwner(initialOwner);
+    }
+
+    function _addrToUint256(address addr) private pure returns (uint256) {
+        return uint256(uint160(addr));
+    }
+
+    function setPool(address pool, address poolInterface) external onlyOwner {
+        bool isNew = _pools.set(pool, _addrToUint256(poolInterface));
+
+        emit SetPool(pool, poolInterface, isNew);
+    }
+}

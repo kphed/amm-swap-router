9824a3ca085f6bd87fa4ce4f9d3baa0e462f6177
kphed <kpcrypto69@gmail.com>
2023-08-31 08:29:10 -0400

    Update deployment script with contract variables

--- script/PoolRegistry.s.sol
+++ script/PoolRegistry.s.sol
@@ -2,5 +2,27 @@
 pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
+import {PoolRegistry} from "src/PoolRegistry.sol";
+import {IStandardPool} from "src/pools/IStandardPool.sol";
+import {CurveCryptoV2} from "src/pools/CurveCryptoV2.sol";
+import {CurveStableSwap} from "src/pools/CurveStableSwap.sol";
+import {UniswapV3Fee500} from "src/pools/UniswapV3Fee500.sol";
 
-contract PoolRegistryScript is Script {}
+contract PoolRegistryScript is Script {
+    address public constant CURVE_CRVUSD_USDT =
+        0x390f3595bCa2Df7d23783dFd126427CCeb997BF4;
+    address public constant CURVE_CRVUSD_USDC =
+        0x4DEcE678ceceb27446b35C672dC7d61F30bAD69E;
+    address public constant CURVE_CRVUSD_ETH_CRV =
+        0x4eBdF703948ddCEA3B11f675B4D1Fba9d2414A14;
+    address public constant CURVE_USDT_WBTC_ETH =
+        0xf5f5B97624542D72A9E06f04804Bf81baA15e2B4;
+    address public constant CURVE_USDC_WBTC_ETH =
+        0x7F86Bf177Dd4F3494b841a37e810A34dD56c829B;
+    address public constant UNISWAP_USDC_ETH =
+        0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
+    address public constant UNISWAP_USDT_ETH =
+        0x11b815efB8f581194ae79006d24E0d814B7697F6;
+    address public constant CRVUSD = 0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E;
+    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+}

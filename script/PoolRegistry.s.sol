6e5bf0545798d1860961dadb5cd7469252722c5e
kphed <kpcrypto69@gmail.com>
2023-09-05 13:10:44 -0400

    Replace linked list with nested array

--- script/PoolRegistry.s.sol
+++ script/PoolRegistry.s.sol
@@ -4,24 +4,23 @@ pragma solidity 0.8.19;
 import "forge-std/Script.sol";
 import {PoolRegistry} from "src/PoolRegistry.sol";
 import {IStandardPool} from "src/pools/IStandardPool.sol";
-import {CurveCryptoV2} from "src/pools/CurveCryptoV2.sol";
 import {CurveStableSwap} from "src/pools/CurveStableSwap.sol";
 import {UniswapV3} from "src/pools/UniswapV3.sol";
+import {CurveStableSwapFactory} from "src/pools/CurveStableSwapFactory.sol";
+import {UniswapV3Factory} from "src/pools/UniswapV3Factory.sol";
 
 contract PoolRegistryScript is Script {
     address public constant CURVE_CRVUSD_USDT =
         0x390f3595bCa2Df7d23783dFd126427CCeb997BF4;
     address public constant CURVE_CRVUSD_USDC =
         0x4DEcE678ceceb27446b35C672dC7d61F30bAD69E;
-    address public constant CURVE_USDT_WBTC_ETH =
-        0xf5f5B97624542D72A9E06f04804Bf81baA15e2B4;
-    address public constant CURVE_USDC_WBTC_ETH =
-        0x7F86Bf177Dd4F3494b841a37e810A34dD56c829B;
     address public constant UNISWAP_USDC_ETH =
         0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
     address public constant UNISWAP_USDT_ETH =
         0x11b815efB8f581194ae79006d24E0d814B7697F6;
     address public constant CRVUSD = 0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     function _encodePath(
@@ -42,59 +41,39 @@ contract PoolRegistryScript is Script {
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        bytes32 crvUSDETH = _hashTokenPair(CRVUSD, WETH);
+        bytes32 ethCRVUSD = _hashTokenPair(WETH, CRVUSD);
+        address[] memory crvUSDETHPools = new address[](2);
+        address[] memory ethCRVUSDPools = new address[](2);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        IStandardPool curveStableSwap = IStandardPool(
-            address(new CurveStableSwap())
+        UniswapV3Factory uniswapV3Factory = new UniswapV3Factory();
+        CurveStableSwapFactory curveStableSwapFactory = new CurveStableSwapFactory();
+        crvUSDETHPools[0] = curveStableSwapFactory.create(
+            CURVE_CRVUSD_USDC,
+            1,
+            0
         );
-        IStandardPool uniswapV3 = IStandardPool(address(new UniswapV3()));
+        crvUSDETHPools[1] = uniswapV3Factory.create(
+            UNISWAP_USDC_ETH,
+            USDC,
+            true
+        );
+        ethCRVUSDPools[0] = uniswapV3Factory.create(
+            UNISWAP_USDC_ETH,
+            WETH,
+            false
+        );
+        ethCRVUSDPools[1] = curveStableSwapFactory.create(
+            CURVE_CRVUSD_USDC,
+            0,
+            1
+        );
         PoolRegistry registry = new PoolRegistry(vm.envAddress("OWNER"));
 
-        address[] memory pools = new address[](4);
-        IStandardPool[] memory poolInterfaces = new IStandardPool[](4);
-        pools[0] = CURVE_CRVUSD_USDT;
-        pools[1] = CURVE_CRVUSD_USDC;
-        pools[2] = UNISWAP_USDC_ETH;
-        pools[3] = UNISWAP_USDT_ETH;
-        poolInterfaces[0] = curveStableSwap;
-        poolInterfaces[1] = curveStableSwap;
-        poolInterfaces[2] = uniswapV3;
-        poolInterfaces[3] = uniswapV3;
-
-        registry.addPools(pools, poolInterfaces);
-
-        bytes32 crvUSDETH = _hashTokenPair(CRVUSD, WETH);
-        address[] memory crvUSDETHPools = new address[](2);
-        crvUSDETHPools[0] = CURVE_CRVUSD_USDC;
-        crvUSDETHPools[1] = UNISWAP_USDC_ETH;
-        uint48[2][] memory crvUSDETHTokenIndexes = new uint48[2][](2);
-        crvUSDETHTokenIndexes[0][0] = 1;
-        crvUSDETHTokenIndexes[0][1] = 0;
-        crvUSDETHTokenIndexes[1][0] = 0;
-        crvUSDETHTokenIndexes[1][1] = 1;
-
-        registry.addExchangePath(
-            crvUSDETH,
-            crvUSDETHPools,
-            crvUSDETHTokenIndexes
-        );
-
-        bytes32 ethCRVUSD = _hashTokenPair(WETH, CRVUSD);
-        address[] memory ethCRVUSDPools = new address[](2);
-        ethCRVUSDPools[0] = UNISWAP_USDC_ETH;
-        ethCRVUSDPools[1] = CURVE_CRVUSD_USDC;
-        uint48[2][] memory ethCRVUSDTokenIndexes = new uint48[2][](2);
-        ethCRVUSDTokenIndexes[0][0] = 1;
-        ethCRVUSDTokenIndexes[0][1] = 0;
-        ethCRVUSDTokenIndexes[1][0] = 0;
-        ethCRVUSDTokenIndexes[1][1] = 1;
-
-        registry.addExchangePath(
-            ethCRVUSD,
-            ethCRVUSDPools,
-            ethCRVUSDTokenIndexes
-        );
+        registry.addExchangePath(crvUSDETH, crvUSDETHPools);
+        registry.addExchangePath(ethCRVUSD, ethCRVUSDPools);
 
         vm.stopBroadcast();
     }
--- src/PoolRegistry.sol
+++ src/PoolRegistry.sol
@@ -3,45 +3,22 @@ pragma solidity 0.8.19;
 
 import {Ownable} from "solady/auth/Ownable.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
-import {Solarray} from "solarray/Solarray.sol";
-import {LinkedList} from "src/lib/LinkedList.sol";
 import {IStandardPool} from "src/pools/IStandardPool.sol";
 
 contract PoolRegistry is Ownable {
-    using LinkedList for LinkedList.List;
     using SafeTransferLib for address;
-    using Solarray for address[];
 
-    struct ExchangePaths {
-        uint256 nextIndex;
-        mapping(uint256 index => LinkedList.List path) paths;
-    }
+    mapping(address pool => bool isSet) public pools;
+    mapping(bytes32 tokenPair => address[][] path) public exchangePaths;
 
-    // Byte offsets for decoding paths (packed ABI encoding).
-    uint256 private constant _PATH_POOL_OFFSET = 20;
-    uint256 private constant _PATH_INPUT_TOKEN_OFFSET = 26;
-    uint256 private constant _PATH_OUTPUT_TOKEN_OFFSET = 32;
-
-    mapping(address pool => mapping(uint256 index => address token) tokens)
-        public poolTokens;
-    mapping(bytes32 tokenPair => ExchangePaths paths) private _exchangePaths;
-
-    event AddPool(address indexed pool, address[] tokens);
     event AddExchangePath(bytes32 indexed tokenPair);
 
-    error Duplicate();
     error InsufficientOutput();
-    error PoolTokenNotSet();
-    error PoolTokensIdentical();
     error UnauthorizedCaller();
     error FailedSwap(bytes);
 
     constructor(address initialOwner) {
         _initializeOwner(initialOwner);
-    }
-
-    function _decodePath(bytes32 path) private pure returns (address pool) {
-        return address(uint160(uint256(path)));
     }
 
     function getOutputAmount(
@@ -52,34 +29,26 @@ contract PoolRegistry is Ownable {
         view
         returns (uint256 bestOutputIndex, uint256 bestOutputAmount)
     {
-        ExchangePaths storage exchangePaths = _exchangePaths[tokenPair];
-        uint256 exchangePathsLength = exchangePaths.nextIndex;
+        address[][] memory _exchangePaths = exchangePaths[tokenPair];
+        uint256 exchangePathsLength = _exchangePaths.length;
         uint256 amount = swapAmount;
 
         // Loop iterator variables are bound by exchange path list lengths and will not overflow.
         unchecked {
             for (uint256 i = 0; i < exchangePathsLength; ++i) {
-                LinkedList.List storage list = exchangePaths.paths[i];
-                bytes32 listKey = list.head;
+                address[] memory paths = _exchangePaths[i];
+                uint256 pathsLength = paths.length;
 
-                while (listKey != bytes32(0)) {
-                    address pool = _decodePath(listKey);
-                    amount = IStandardPool(pool).quoteTokenOutput(amount);
-
-                    if (listKey == list.tail) {
-                        // Compare the latest output amount against the current best output amount.
-                        if (amount > bestOutputAmount) {
-                            bestOutputIndex = i;
-                            bestOutputAmount = amount;
-                        }
-
-                        amount = swapAmount;
-
-                        break;
-                    }
-
-                    listKey = list.elements[listKey].nextKey;
+                for (uint256 j = 0; j < pathsLength; ++j) {
+                    amount = IStandardPool(paths[j]).quoteTokenOutput(amount);
                 }
+
+                if (amount > bestOutputAmount) {
+                    bestOutputIndex = i;
+                    bestOutputAmount = amount;
+                }
+
+                amount = swapAmount;
             }
         }
     }
@@ -88,35 +57,30 @@ contract PoolRegistry is Ownable {
         bytes32 tokenPair,
         uint256 swapAmount
     ) external view returns (uint256 bestInputIndex, uint256 bestInputAmount) {
-        ExchangePaths storage exchangePaths = _exchangePaths[tokenPair];
-        uint256 exchangePathsLength = exchangePaths.nextIndex;
+        address[][] memory _exchangePaths = exchangePaths[tokenPair];
+        uint256 exchangePathsLength = _exchangePaths.length;
         uint256 amount = swapAmount;
 
         // Loop iterator variables are bound by exchange path list lengths and will not overflow.
         unchecked {
             for (uint256 i = 0; i < exchangePathsLength; ++i) {
-                LinkedList.List storage list = exchangePaths.paths[i];
-                bytes32 listKey = list.tail;
+                address[] memory paths = _exchangePaths[i];
+                uint256 pathsLength = paths.length;
 
-                while (listKey != bytes32(0)) {
-                    address pool = _decodePath(listKey);
-                    amount = IStandardPool(pool).quoteTokenInput(amount);
+                while (pathsLength != 0) {
+                    --pathsLength;
 
-                    if (listKey == list.head) {
-                        // Compare the latest input amount against the current best input amount.
-                        // If the best input amount is unset, set it.
-                        if (amount < bestInputAmount || bestInputAmount == 0) {
-                            bestInputIndex = i;
-                            bestInputAmount = amount;
-                        }
-
-                        amount = swapAmount;
-
-                        break;
-                    }
-
-                    listKey = list.elements[listKey].previousKey;
+                    amount = IStandardPool(paths[pathsLength]).quoteTokenInput(
+                        amount
+                    );
                 }
+
+                if (amount < bestInputAmount || bestInputAmount == 0) {
+                    bestInputIndex = i;
+                    bestInputAmount = amount;
+                }
+
+                amount = swapAmount;
             }
         }
     }
@@ -130,24 +94,24 @@ contract PoolRegistry is Ownable {
     ) external returns (uint256) {
         inputToken.safeTransferFrom(msg.sender, address(this), swapAmount);
 
-        LinkedList.List storage list = _exchangePaths[
+        address[] memory paths = exchangePaths[
             keccak256(abi.encodePacked(inputToken, outputToken))
-        ].paths[pathIndex];
-        bytes32 listKey = list.head;
+        ][pathIndex];
+        uint256 pathsLength = paths.length;
 
-        while (listKey != bytes32(0)) {
-            address pool = _decodePath(listKey);
-            (bool success, bytes memory data) = pool.delegatecall(
-                abi.encodeWithSelector(IStandardPool.swap.selector, swapAmount)
-            );
+        unchecked {
+            for (uint256 i = 0; i < pathsLength; ++i) {
+                (bool success, bytes memory data) = paths[i].delegatecall(
+                    abi.encodeWithSelector(
+                        IStandardPool.swap.selector,
+                        swapAmount
+                    )
+                );
 
-            if (!success) revert FailedSwap(data);
+                if (!success) revert FailedSwap(data);
 
-            swapAmount = abi.decode(data, (uint256));
-
-            if (listKey == list.tail) break;
-
-            listKey = list.elements[listKey].nextKey;
+                swapAmount = abi.decode(data, (uint256));
+            }
         }
 
         if (swapAmount < minOutputTokenAmount) revert InsufficientOutput();
@@ -160,28 +124,27 @@ contract PoolRegistry is Ownable {
     function addExchangePath(
         bytes32 tokenPair,
         address[] calldata interfaces
-    ) public onlyOwner {
-        ExchangePaths storage exchangePaths = _exchangePaths[tokenPair];
-        LinkedList.List storage exchangePathsList = exchangePaths.paths[
-            exchangePaths.nextIndex
-        ];
+    ) external onlyOwner {
+        address[][] storage _exchangePaths = exchangePaths[tokenPair];
+        uint256 exchangePathsLength = _exchangePaths.length;
+
+        _exchangePaths.push();
+
+        address[] storage paths = _exchangePaths[exchangePathsLength];
         uint256 interfacesLength = interfaces.length;
 
         unchecked {
-            ++exchangePaths.nextIndex;
-
             for (uint256 i = 0; i < interfacesLength; ++i) {
                 address poolInterface = interfaces[i];
                 address[] memory tokens = IStandardPool(poolInterface).tokens();
                 address pool = IStandardPool(poolInterface).pool();
+                pools[pool] = true;
 
                 for (uint256 j = 0; j < tokens.length; ++j) {
                     tokens[j].safeApproveWithRetry(pool, type(uint256).max);
                 }
 
-                exchangePathsList.push(
-                    bytes32(uint256(uint160(poolInterface)))
-                );
+                paths.push(poolInterface);
             }
         }
 
@@ -193,6 +156,8 @@ contract PoolRegistry is Ownable {
         int256 amount1Delta,
         bytes calldata data
     ) external {
+        if (!pools[msg.sender]) revert UnauthorizedCaller();
+
         address inputToken = abi.decode(data, (address));
 
         if (amount0Delta > 0) {

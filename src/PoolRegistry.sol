9ae2ffa68e21c4e109b4e4bba68ccd46f250f484
kphed <kpcrypto69@gmail.com>
2023-09-05 11:18:11 -0400

    Update PoolRegistry to use new contract-based paths

--- src/PoolRegistry.sol
+++ src/PoolRegistry.sol
@@ -22,7 +22,6 @@ contract PoolRegistry is Ownable {
     uint256 private constant _PATH_INPUT_TOKEN_OFFSET = 26;
     uint256 private constant _PATH_OUTPUT_TOKEN_OFFSET = 32;
 
-    mapping(address pool => IStandardPool poolInterface) public poolInterfaces;
     mapping(address pool => mapping(uint256 index => address token) tokens)
         public poolTokens;
     mapping(bytes32 tokenPair => ExchangePaths paths) private _exchangePaths;
@@ -41,63 +40,8 @@ contract PoolRegistry is Ownable {
         _initializeOwner(initialOwner);
     }
 
-    function _encodePath(
-        address pool,
-        uint48 inputTokenIndex,
-        uint48 outputTokenIndex
-    ) private pure returns (bytes32) {
-        return
-            bytes32(abi.encodePacked(pool, inputTokenIndex, outputTokenIndex));
-    }
-
-    function _decodePath(
-        bytes32 path
-    )
-        private
-        pure
-        returns (address pool, uint48 inputTokenIndex, uint48 outputTokenIndex)
-    {
-        bytes memory _path = abi.encodePacked(path);
-
-        assembly {
-            pool := mload(add(_path, _PATH_POOL_OFFSET))
-            inputTokenIndex := mload(add(_path, _PATH_INPUT_TOKEN_OFFSET))
-            outputTokenIndex := mload(add(_path, _PATH_OUTPUT_TOKEN_OFFSET))
-        }
-    }
-
-    function getExchangePaths(
-        bytes32 tokenPair
-    )
-        external
-        view
-        returns (
-            uint256 nextIndex,
-            address[][] memory pools,
-            uint256[2][] memory tokenIndexes
-        )
-    {
-        ExchangePaths storage exchangePaths = _exchangePaths[tokenPair];
-        nextIndex = exchangePaths.nextIndex;
-        pools = new address[][](nextIndex);
-        tokenIndexes = new uint256[2][](nextIndex);
-
-        for (uint256 i = 0; i < nextIndex; ++i) {
-            LinkedList.List storage list = exchangePaths.paths[i];
-            bytes32 listKey = list.head;
-
-            while (listKey != bytes32(0)) {
-                (
-                    address pool,
-                    uint256 inputTokenIndex,
-                    uint256 outputTokenIndex
-                ) = _decodePath(listKey);
-                pools[i] = pools[i].append(pool);
-                tokenIndexes[i][0] = inputTokenIndex;
-                tokenIndexes[i][1] = outputTokenIndex;
-                listKey = list.elements[listKey].nextKey;
-            }
-        }
+    function _decodePath(bytes32 path) private pure returns (address pool) {
+        return address(uint160(uint256(path)));
     }
 
     function getOutputAmount(
@@ -110,6 +54,7 @@ contract PoolRegistry is Ownable {
     {
         ExchangePaths storage exchangePaths = _exchangePaths[tokenPair];
         uint256 exchangePathsLength = exchangePaths.nextIndex;
+        uint256 amount = swapAmount;
 
         // Loop iterator variables are bound by exchange path list lengths and will not overflow.
         unchecked {
@@ -118,27 +63,22 @@ contract PoolRegistry is Ownable {
                 bytes32 listKey = list.head;
 
                 while (listKey != bytes32(0)) {
-                    (
-                        address pool,
-                        uint48 inputTokenIndex,
-                        uint48 outputTokenIndex
-                    ) = _decodePath(listKey);
-                    swapAmount = poolInterfaces[pool].quoteTokenOutput(
-                        pool,
-                        inputTokenIndex,
-                        outputTokenIndex,
-                        swapAmount
-                    );
+                    address pool = _decodePath(listKey);
+                    amount = IStandardPool(pool).quoteTokenOutput(amount);
 
-                    if (listKey == list.tail) break;
+                    if (listKey == list.tail) {
+                        amount = swapAmount;
+
+                        break;
+                    }
 
                     listKey = list.elements[listKey].nextKey;
                 }
 
                 // Compare the latest output amount against the current best output amount.
-                if (swapAmount > bestOutputAmount) {
+                if (amount > bestOutputAmount) {
                     bestOutputIndex = i;
-                    bestOutputAmount = swapAmount;
+                    bestOutputAmount = amount;
                 }
             }
         }
@@ -150,6 +90,7 @@ contract PoolRegistry is Ownable {
     ) external view returns (uint256 bestInputIndex, uint256 bestInputAmount) {
         ExchangePaths storage exchangePaths = _exchangePaths[tokenPair];
         uint256 exchangePathsLength = exchangePaths.nextIndex;
+        uint256 amount = swapAmount;
 
         // Loop iterator variables are bound by exchange path list lengths and will not overflow.
         unchecked {
@@ -158,28 +99,23 @@ contract PoolRegistry is Ownable {
                 bytes32 listKey = list.tail;
 
                 while (listKey != bytes32(0)) {
-                    (
-                        address pool,
-                        uint48 inputTokenIndex,
-                        uint48 outputTokenIndex
-                    ) = _decodePath(listKey);
-                    swapAmount = poolInterfaces[pool].quoteTokenInput(
-                        pool,
-                        inputTokenIndex,
-                        outputTokenIndex,
-                        swapAmount
-                    );
+                    address pool = _decodePath(listKey);
+                    amount = IStandardPool(pool).quoteTokenInput(amount);
 
-                    if (listKey == list.head) break;
+                    if (listKey == list.head) {
+                        amount = swapAmount;
+
+                        break;
+                    }
 
                     listKey = list.elements[listKey].previousKey;
                 }
 
                 // Compare the latest input amount against the current best input amount.
                 // If the best input amount is unset, set it.
-                if (swapAmount < bestInputAmount || bestInputAmount == 0) {
+                if (amount < bestInputAmount || bestInputAmount == 0) {
                     bestInputIndex = i;
-                    bestInputAmount = swapAmount;
+                    bestInputAmount = amount;
                 }
             }
         }
@@ -200,21 +136,10 @@ contract PoolRegistry is Ownable {
         bytes32 listKey = list.head;
 
         while (listKey != bytes32(0)) {
-            (
-                address pool,
-                uint256 inputTokenIndex,
-                uint256 outputTokenIndex
-            ) = _decodePath(listKey);
-            (bool success, bytes memory data) = address(poolInterfaces[pool])
-                .delegatecall(
-                    abi.encodeWithSelector(
-                        IStandardPool.swap.selector,
-                        pool,
-                        inputTokenIndex,
-                        outputTokenIndex,
-                        swapAmount
-                    )
-                );
+            address pool = _decodePath(listKey);
+            (bool success, bytes memory data) = pool.delegatecall(
+                abi.encodeWithSelector(IStandardPool.swap.selector, swapAmount)
+            );
 
             if (!success) revert FailedSwap(data);
 
@@ -232,82 +157,30 @@ contract PoolRegistry is Ownable {
         return swapAmount;
     }
 
-    /**
-     * @notice Add a liquidity pool.
-     * @param  pool           address        Liquidity pool address.
-     * @param  poolInterface  IStandardPool  Liquidity pool interface.
-     */
-    function addPool(
-        address pool,
-        IStandardPool poolInterface
-    ) public onlyOwner {
-        // Should not allow redundant pools from being added.
-        if (address(poolInterfaces[pool]) != address(0)) revert Duplicate();
-
-        // Store the new pool, along with the number of tokens in it.
-        poolInterfaces[pool] = poolInterface;
-
-        address[] memory tokens = poolInterface.tokens(pool);
-        uint256 tokensLength = tokens.length;
-
-        unchecked {
-            for (uint256 i = 0; i < tokensLength; ++i) {
-                // Pre-approve pools to spend tokens in order to save gas.
-                tokens[i].safeApprove(pool, type(uint256).max);
-
-                poolTokens[pool][i] = tokens[i];
-            }
-        }
-
-        emit AddPool(pool, tokens);
-    }
-
-    function addPools(
-        address[] calldata pools,
-        IStandardPool[] calldata interfaces
-    ) external onlyOwner {
-        uint256 poolsLength = pools.length;
-
-        for (uint256 i = 0; i < poolsLength; ) {
-            addPool(pools[i], interfaces[i]);
-
-            unchecked {
-                ++i;
-            }
-        }
-    }
-
     function addExchangePath(
         bytes32 tokenPair,
-        address[] calldata pools,
-        uint48[2][] calldata tokenIndexes
+        address[] calldata interfaces
     ) public onlyOwner {
         ExchangePaths storage exchangePaths = _exchangePaths[tokenPair];
         LinkedList.List storage exchangePathsList = exchangePaths.paths[
             exchangePaths.nextIndex
         ];
-        uint256 poolsLength = pools.length;
+        uint256 interfacesLength = interfaces.length;
 
         unchecked {
             ++exchangePaths.nextIndex;
 
-            for (uint256 i = 0; i < poolsLength; ++i) {
-                address pool = pools[i];
-                uint48 inputTokenIndex = tokenIndexes[i][0];
-                uint48 outputTokenIndex = tokenIndexes[i][1];
+            for (uint256 i = 0; i < interfacesLength; ++i) {
+                address poolInterface = interfaces[i];
+                address[] memory tokens = IStandardPool(poolInterface).tokens();
+                address pool = IStandardPool(poolInterface).pool();
 
-                // Throws if the token indexes are invalid (e.g. pool not set, or token index invalid).
-                if (poolTokens[pool][inputTokenIndex] == address(0))
-                    revert PoolTokenNotSet();
-                if (poolTokens[pool][outputTokenIndex] == address(0))
-                    revert PoolTokenNotSet();
-
-                // Unless we're arbing (we're not), there should be no reason to swap the same tokens.
-                if (inputTokenIndex == outputTokenIndex)
-                    revert PoolTokensIdentical();
+                for (uint256 j = 0; j < tokens.length; ++j) {
+                    tokens[j].safeApproveWithRetry(pool, type(uint256).max);
+                }
 
                 exchangePathsList.push(
-                    _encodePath(pool, inputTokenIndex, outputTokenIndex)
+                    bytes32(uint256(uint160(poolInterface)))
                 );
             }
         }
@@ -320,9 +193,6 @@ contract PoolRegistry is Ownable {
         int256 amount1Delta,
         bytes calldata data
     ) external {
-        if (address(poolInterfaces[msg.sender]) == address(0))
-            revert UnauthorizedCaller();
-
         address inputToken = abi.decode(data, (address));
 
         if (amount0Delta != 0) {
